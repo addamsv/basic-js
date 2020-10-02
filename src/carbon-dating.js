33b6f06504460e66233200ad4bce8804062b2475
addamsv <swares@mail.ru>
2020-10-02 23:21:12 +0300

    feat: carbon d and dream team is done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,11 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sampleActivity) {
+  if(!sampleActivity || typeof(sampleActivity) !== 'string' || sampleActivity*1 !== 'number'){
+    return false;
+  }
+  return Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD);
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
