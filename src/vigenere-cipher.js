71dd1288df463727346b52e831384e4b2b63dcd5
addamsv <swares@mail.ru>
2020-10-09 11:21:59 +0300

    refactor: separated code to many different little functions

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,80 +3,86 @@ const CustomError = require("../extensions/custom-error");
 class VigenereCipheringMachine {
 
 	constructor(_forwardWay=true) {
-		this.forwardWay = _forwardWay;
-	}
-  getTable() {
-    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-    var table = [];
-    for (var i = 0; i < alphabet.length; i++) {
-        table[i] = alphabet.slice(i).concat(alphabet.slice(0, i));
-    }
-    return table;
+    this.forwardWay = _forwardWay;
+    this.alphabet = this.getAlphabet();
+    this.table = this.getTable();
   }
+
   encrypt(message, key) {
-    if(message===undefined || key===undefined){
-      throw new error();
-    }
-    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-    const table = this.getTable();
-    var cryptStr = '';
+    this.validateParams(message,key);
+    let cryptStr = '';
     message = message.toUpperCase();
     key = key.toUpperCase();
-    for (var i = 0,j = 0; i < message.length; i++, j++) {
-      if(alphabet.indexOf(key[i]) === -1 ){//&& this.forwardWay
+    for (let i = 0,j = 0; i < message.length; i++, j++) {
+      if(this.isSymbNooootExstInAlphabet(key[i])){
         key += key;
-        // if(!this.forwardWay){
-        //   console.log(key);
-        // }
       }
-      if(alphabet.indexOf(message[i]) === -1){
+      if(this.isSymbNooootExstInAlphabet(message[i])){
         cryptStr += message[i];
         j--;
         continue;
       }
-      cryptStr += table[alphabet.indexOf(message[i])][alphabet.indexOf(key[j])];
+      /* getingNextSymba - table[ind1][ind2] */
+      cryptStr += this.table[this.alphabet.indexOf(message[i])][this.alphabet.indexOf(key[j])];
     }
-    if(!this.forwardWay){
-      // console.log('forwardWay: '+this.forwardWay);
-      // console.log('message: '+message+'; cryptStr: '+cryptStr.split('').reverse().join('')+'\n');
-      return cryptStr.split('').reverse().join('');
-    }
-    // console.log('message: '+message+'; cryptStr: '+cryptStr);
-    return cryptStr;
-  }    
+    return this.getFinishData(cryptStr);;
+  }
+
   decrypt(message, key) {
-    if(message===undefined || key===undefined){
-      throw new error();
-    }
-    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-    var decryptStr = '';
-    const table = this.getTable();
+    this.validateParams(message,key);
+    let decryptStr = '';
     key = key.toUpperCase();
-    for (var i = 0,j = 0; i < message.length; i++, j++) {
-      if(alphabet.indexOf(key[i]) === -1){// && this.forwardWay
+    for (let i = 0,j = 0; i < message.length; i++, j++) {
+      if(this.isSymbNooootExstInAlphabet(key[i])){
         key += key;
-        // if(!this.forwardWay){
-        //   console.log(key);
-        // }
       }
-      if(alphabet.indexOf(message[i]) === -1){
+      if(this.isSymbNooootExstInAlphabet(message[i])){
         decryptStr += message[i];
         j--;
         continue;
       }
-      var 
-       row = alphabet.indexOf(key[j]),
-       coll = table[row].indexOf(message[i]);
+      decryptStr += this.getSymbaFromAlphabet(message[i],key[j]);
+    }
+    return this.getFinishData(decryptStr);
+  }
   
-      decryptStr += alphabet[coll];
+  
+  /* Private area */
+
+  getTable() {
+    let table = [];
+    for (var i = 0; i < this.alphabet.length; i++) {
+        table[i] = this.alphabet.slice(i).concat(this.alphabet.slice(0, i));
     }
-    if(!this.forwardWay){
-      // console.log('forwardWay: '+this.forwardWay);
-      // console.log('message: '+message+'; decryptStr: '+decryptStr.split('').reverse().join('')+'\n');
-      return decryptStr.split('').reverse().join('');
-    }
-    // console.log('message: '+message+'; decryptStr: '+decryptStr+'\n');
-    return decryptStr;
+    return table;
+  }
+
+  getAlphabet(a='eng') {
+    return "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  }
+  
+  isSymbNooootExstInAlphabet(symba){
+    return this.alphabet.indexOf(symba) === -1;
+  }
+
+  getFinishData(str){
+    return (!this.forwardWay) ? str.split('').reverse().join('') : str;
+  }
+
+  getSymbaFromAlphabet(i,j){
+    return this.alphabet[this.getSybaPosFromTable(i, j)];
+  }
+
+  getSybaPosFromTable(messagePos, keyPos){
+    return this.table[this.getRow(keyPos)].indexOf(messagePos);
+  }
+  getRow(keyPos){
+    return this.alphabet.indexOf(keyPos);
+  }
+  validateParams(m,k){
+    if(m===undefined || k===undefined){
+      throw new error();
+    }  
   }
 }
 
